2ea4618f7ea2ebe851522cd0f17f0604b52cc2a9
Osmiun-enfec <divyanshu.singh@enfec.com>
2025-11-25 17:59:27 +0530

    decorators commit

--- hello.py
+++ hello.py
@@ -1,1 +1,14 @@
-print ("Hello World!")
+def greet(fx):
+    def mfx(*args, **kwargs):
+        print ("Good Morning")
+        fx(*args, **kwargs)
+        print ("Thank You for using this function")
+    return mfx
+
+@greet
+def add(x, y):
+    z = x + y
+    print ("The sum is:", z)
+
+add(4, 5)
+#print ("Hello World!")
